066b185e7b36be14044ec2f8d84d224fe267cf75
nikkashid <nikhil.kashid1@gmail.com>
2020-08-18 18:23:51 +0530

    removing comment lines and extra classes.

--- app/src/main/java/com/nikhil/hiltwithdatabinding/responses/Reponse.kt
+++
@@ -1,3 +0,0 @@
-package com.nikhil.hiltwithdatabinding.responses
-
-class Reponse : ArrayList<ReponseItem>()
--- app/src/main/java/com/nikhil/hiltwithdatabinding/ui/adapters/HerosHomeAdapter.kt
+++ app/src/main/java/com/nikhil/hiltwithdatabinding/ui/adapters/HerosHomeAdapter.kt
@@ -6,24 +6,18 @@ import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.nikhil.hiltwithdatabinding.R
 import com.nikhil.hiltwithdatabinding.databinding.ItemHerosListBinding
 import com.nikhil.hiltwithdatabinding.responses.HeroResponse
-import kotlinx.android.synthetic.main.item_heros_list.view.*
 
 
-class HerosHomeAdapter :
-    ListAdapter<HeroResponse, HerosHomeAdapter.HerosViewHolder> {
+class HerosHomeAdapter() :
+    ListAdapter<HeroResponse, HerosHomeAdapter.HerosViewHolder>(differCallback) {
 
     inner class HerosViewHolder(itemView: ItemHerosListBinding) :
         RecyclerView.ViewHolder(itemView.root) {
 
         var binding: ItemHerosListBinding = itemView
-
-    }
-
-    constructor() : super(differCallback) {
 
     }
 
@@ -39,14 +33,6 @@ class HerosHomeAdapter :
     override fun onBindViewHolder(holder: HerosViewHolder, position: Int) {
         val hero = getItem(position)
         holder.binding.homeAdapterItem = hero
-        /*holder.itemView.apply {
-            Glide.with(this).load(hero.imageurl).into(iv_heroImage)
-            tv_name.text = hero.name
-            tv_Team.text = hero.team
-            tv_realName.text = hero.realname
-            tv_publisher.text = hero.publisher
-            tv_createdBy.text = hero.createdby
-        }*/
     }
 
     companion object {
